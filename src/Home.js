dfde42eaf47adb41f46f4afd0151bbbde72d3bdc
Dag <Dghistania@merrimack.edu>
2019-12-12 16:22:54 +0300

    routes

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <br/>
+                <br/>
+                <h1>انت في البيت</h1>
+            </div>
+
+        )
+    }
+}
+
+export default Home
