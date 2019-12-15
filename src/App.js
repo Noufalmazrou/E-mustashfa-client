d5bd3a7eaa282a34dbae2d4041da0151770a4d21
diala.ghamdi@gmail.com <you@example.com>
2019-12-15 09:54:37 +0300

    Home part 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Appointments  from './Appointments/Appointments'
+import Hospital from './Appointments/Hospital'
+import Doctors from './Appointments/Doctors'
+import Sections from './Appointments/Sections'
+import Home from './Home/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home/>
+
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Appointments/Hospital.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Card, Icon } from 'semantic-ui-react'
+
+export default class Hospital extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
