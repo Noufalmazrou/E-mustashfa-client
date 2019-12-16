48241a290c4b2a090ed922589550a761688274cb
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-12-16 13:35:51 +0300

    WOOHOOO :))) I created the firebase uploader!!

--- src/uploading/Upload.jsx
+++ src/uploading/Upload.jsx
@@ -1,33 +1,59 @@
 import React, { Component } from 'react'
-import firebase from 'firebase/app';
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMIAN,
-    databaseURL: process.env.DATABASEURL,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGE,
-    appId: process.env.APPID,
-    measurementId: process.env.MEASURMENTID
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+import {storage} from './config'
   
 export default class Upload extends Component {
-    files = (e)=>{
-        var fileList = e.target.files;
-        var fileName = fileList[0].name
-        console.log(fileList[0].name);
-        var storage = firebase.storage('gs://hakunamata-52f67.appspot.com').ref(fileName).put();
-
+    constructor(){
+        super() 
+        this.state= {
+            uploded_img: null,
+            img_url: null
+        }
+        this.FileSelectedHandler = this.FileSelectedHandler.bind(this)
+        this.fileUploadHandler =  this.fileUploadHandler.bind(this)
     }
+    FileSelectedHandler = event => {
+        if(event.target.files[0]){
+          const img = event.target.files[0];
+          this.setState({
+            uploded_img:img
+          })
+          console.log(this.state.uploded_img);
+          console.log(img);
+          
+        }
+    }
+    fileUploadHandler(){
+        console.log('hiiiiii');
+        
+        const {uploded_img} = this.state
+        console.log(uploded_img)
+        const UploadTask = storage.ref(`prodimg/${uploded_img.name}`).put(uploded_img)
+        console.log(uploded_img.name);
+        
+         UploadTask.on('state_changed',
+        (snapshot)=>{
+        },
+        (error)=>{
+          console.log(error);
+        },
+      ()=>{
+        storage.ref('prodimg').child(uploded_img.name).getDownloadURL().then(url=>{
+         this.setState({
+           img_url:url
+         })
+        })
+      })
+      }
     render() {
         return (
             <div>
                 
-                <input type="file" name="files" id="" onChange={this.files}/>
+                <input type="file" name="files" id="" onChange={this.FileSelectedHandler}/>
+                <button onClick={()=>this.fileUploadHandler()}>idlkjklgjoieqrjlkgjk</button>
+                <button onClick={()=>console.log(this.state.img_url)
+                } >priny</button>
+                   <button onClick={()=>console.log(this.state.uploded_img)
+                } >uploded</button>
             </div>
         )
     }
