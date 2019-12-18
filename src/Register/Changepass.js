93857da6aa52800f6343f829660dc55245679d45
“Nouf <nouf_mazrou@hotmail.com>
2019-12-18 15:55:03 +0300

    signin-signup

--- src/Register/Changepass.js
+++ src/Register/Changepass.js
@@ -1,29 +1,74 @@
-import React, { Component } from 'react'
-import { Form, Input } from 'semantic-ui-react'
-import "semantic-ui-css/semantic.min.css"
+import React, { Component } from 'react'
+import { Form, Input, Label, Divider } from 'semantic-ui-react'
+import "semantic-ui-css/semantic.min.css"
+import jwt from 'jsonwebtoken'
+import axios from 'axios'
 
-export default class Changepass extends Component {
-    render() {
-        return (
-           
-                    <>
-<div className = "formSing">
- 
+export default class Changepass extends Component {
+  state ={
+rightPass : null,
 
-  
-          <Form onSubmit = {this.submit} action={`http://localhost:5100/users/changepassword/${localStorage.usertoken}`} method="post">
-    
-        
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password"  name="newPassword" onChange={this.onChange}/>
-          </Form.Group>
-        
-          <Form.Button>Submit</Form.Button>
-        </Form>
-        </div>
-        </>
-          
-        )
-    }
+  }
+
+  componentDidMount(){
+
+var decoded = jwt.verify(localStorage.usertoken , "secret")
+this.setState({
+  email : decoded.user.email
+})
+  }
+  onChange = (e)=>{
+
+    if (e.target.value.length > 0){
+      this.setState({
+        [e.target.name] : e.target.value,
+        [e.target.name +"Lable"] : false
+      })
+    }else {
+      this.setState({
+        [e.target.name] : e.target.value,
+        [e.target.name +"Lable"] : true
+      })
+    }
+    }
+
+    onSunmit =()=>{
+      console.log("uu");
+      
+      axios.post('http://localhost:4000/user/check' , {
+        email : this.state.email,
+        oldPassword : this.state.oldPassword,
+        newPassword : this.state.newPassword
+      })
+      .then(res => this.setState({rightPass : res.data}))
+
+
+    }
+  render() {
+    console.log(this.state);
+    
+    return (
+
+      <div className="formSing" >
+     
+       
+      <Form 
+      onSubmit = {this.onSunmit} >
+    
+        <Form.Field inline style={{backgroundColor:'#B0C4DE'}}>
+          <input type='text' placeholder='oldPassword' name = "oldPassword" onChange={this.onChange} />
+          {!this.state.passwordLable &&<Label pointing='left'>worng password</Label>}
+        </Form.Field>
+        <Divider />
+    
+       {this.state.rightPass && <Form.Field inline style={{backgroundColor:'#B0C4DE'}}>
+          {this.state.passwordLable &&<Label pointing='right'>Your password must be 6 characters or more</Label>}
+          <input type='password' placeholder='newPaswword' name= "newPaswword" onChange={this.onChange} />
+        </Form.Field>}
+        <Form.Button >check</Form.Button>
+      </Form>
+    
+          </div>
+    )
+  }
 }
